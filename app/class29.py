38b6d63b0985402482fb2bd3f8a415a72c5d1cae
nirfish9 <73849508+nirfish9@users.noreply.github.com>
2020-12-31 18:41:08 +0200

    Update class29.py

--- app/class29.py
+++ app/class29.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def message():
-    return "class29 Flask app"
+    return "class29 Flask app 2"
 
 app.run(host="0.0.0.0", port=8089)
